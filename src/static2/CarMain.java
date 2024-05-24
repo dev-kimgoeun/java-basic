2d20b001ddc4e5a3f62777ed4796a731e95df99d
KIMGOEUN <ak0150a01@gmail.com>
2024-05-24 14:45:54 +0900

    math

---
+++ src/static2/CarMain.java
@@ -0,0 +1,12 @@
+package static2;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        Car car1 = new Car("k3");
+        Car car2 = new Car("g80");
+        Car car3 = new Car("model Y");
+
+        Car.showTotalCars();
+    }
+}
