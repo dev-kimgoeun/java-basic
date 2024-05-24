5508c954cccd1e3783dfe03018eb9cb05faedbb6
KIMGOEUN <ak0150a01@gmail.com>
2024-05-24 18:26:52 +0900

    final

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10; //static final이 붙으면 관례상 대분자로 작성
+    final  int value =10;
+
+//    public FieldInit(int value){
+//        this.value = value;
+//    } 값이 한 번 초기화 되면 생성자로 사용 불가
+}
